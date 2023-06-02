c74d5c922f0c28bdf8f08d4ea8ab78638d139a2b
fan <fanhouin@gmail.com>
2023-06-03 03:42:45 +0800

    update test

--- CIServer_test.go
+++ CIServer_test.go
@@ -1,73 +1,80 @@
 package main
 
 import (
+	"bytes"
+	"io"
+	"log"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"os"
-	"strings"
+	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGotCpuRoute(t *testing.T) {
-	originalDir, err := os.Getwd()
+// func TestGotCpuRoute(t *testing.T) {
+// 	r := SetupRouter()
+
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("GET", "/api/gotcpu", nil)
+// 	r.ServeHTTP(w, req)
+
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	// assert.Equal(t, "pong", w.Body.String())
+// }
+
+func sendFileRequest(filePath string) *http.Request {
+	file, err := os.Open(filePath)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
-	defer os.Chdir(originalDir)
+	defer file.Close()
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
 
-	r := SetupRouter()
+	fileField, err := writer.CreateFormFile("file", filepath.Base(filePath))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = io.Copy(fileField, file)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/api/gotcpu", nil)
-	r.ServeHTTP(w, req)
+	writer.Close()
 
-	assert.Equal(t, http.StatusOK, w.Code)
-	// assert.Equal(t, "pong", w.Body.String())
+	req, _ := http.NewRequest("POST", "/api/runAFLPlusPlus", body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	return req
 }
 
 func TestRunAFLPlusPlusCrashRoute(t *testing.T) {
-	originalDir, err := os.Getwd()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Chdir(originalDir)
-
+	t.Parallel()
 	r := SetupRouter()
 
-	data := url.Values{"target": {"readfile"}}
-	body := strings.NewReader(data.Encode())
-
+	req := sendFileRequest("c_program/readfile/src/readfile.c")
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/api/runAFLPlusPlus", body)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	r.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "{\"message\":\"Crash Found\"}", w.Body.String())
+	assert.Equal(t, "{\"message\":\"readfile.c : Crash Found\"}", w.Body.String())
 }
 
 func TestRunAFLPlusPlusNotCrashRoute(t *testing.T) {
-	originalDir, err := os.Getwd()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Chdir(originalDir)
-
+	t.Parallel()
+	time.Sleep(5 * time.Second)
 	r := SetupRouter()
 
-	data := url.Values{"target": {"hello"}}
-	body := strings.NewReader(data.Encode())
-
+	req := sendFileRequest("c_program/hello/src/hello.c")
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/api/runAFLPlusPlus", body)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	r.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "{\"message\":\"Crash Not Found\"}", w.Body.String())
+	assert.Equal(t, "{\"message\":\"hello.c : Crash Not Found\"}", w.Body.String())
 }
